dea48f108e475f41c2bb515f33a5aa74c45cd759
Alis Durna <84575898+ALDOR99@users.noreply.github.com>
2023-06-23 17:27:28 +0300

    0.0

--- server/index.js
+++ server/index.js
@@ -3,12 +3,20 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 // --------------------------------
 const app = express();
-dotenv.config;
-// const connect = {
-//     mongoose.connect("")
-// };
+dotenv.config();
+const connect = () => {
+  mongoose
+    .connect(process.env.MONGO)
+    .then(() => {
+      console.log("Connected to DB");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
 // --------------------------------
 
 app.listen(8800, () => {
-  console.log("Connected!");
+  connect();
+  console.log("Connected to Server");
 });
