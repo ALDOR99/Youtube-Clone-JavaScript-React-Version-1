edbb47175577cc39df8392341086e69049c335c6
Alis Durna <84575898+ALDOR99@users.noreply.github.com>
2023-06-23 16:58:52 +0300

    0.0

--- server/index.js
+++ server/index.js
@@ -1,7 +1,14 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 // --------------------------------
 const app = express();
+dotenv.config;
+// const connect = {
+//     mongoose.connect("")
+// };
 // --------------------------------
+
 app.listen(8800, () => {
   console.log("Connected!");
 });
