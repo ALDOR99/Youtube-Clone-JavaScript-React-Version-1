bac0fe6a5439f6bdd015ce52327b4a8fd6d63bcd
Alis Durna <84575898+ALDOR99@users.noreply.github.com>
2023-07-03 14:36:48 +0300

    0.1

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,12 +1,28 @@
 import React from "react";
 import styled from "styled-components";
 import a from "../img/logo.png";
+import HomeIcon from "@mui/icons-material/Home";
+import ExploreOutlinedIcon from "@mui/icons-material/ExploreOutlined";
+import SubscriptionsOutlinedIcon from "@mui/icons-material/SubscriptionsOutlined";
+import VideoLibraryOutlinedIcon from "@mui/icons-material/VideoLibraryOutlined";
+import HistoryOutlinedIcon from "@mui/icons-material/HistoryOutlined";
+import LibraryMusicOutlinedIcon from "@mui/icons-material/LibraryMusicOutlined";
+import SportsEsportsOutlinedIcon from "@mui/icons-material/SportsEsportsOutlined";
+import SportsBasketballOutlinedIcon from "@mui/icons-material/SportsBasketballOutlined";
+import MovieOutlinedIcon from "@mui/icons-material/MovieOutlined";
+import ArticleOutlinedIcon from "@mui/icons-material/ArticleOutlined";
+import LiveTvOutlinedIcon from "@mui/icons-material/LiveTvOutlined";
+// import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import FlagOutlinedIcon from "@mui/icons-material/FlagOutlined";
+import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
 // --------------------------------------
 const Container = styled.div`
   flex: 1;
   background-color: #202020;
   height: 100vh;
   color: white;
+  font-size: 14px;
 `;
 const Wrapper = styled.div`
   padding: 18px 26px;
@@ -15,10 +31,20 @@ const Logo = styled.div`
   display: flex;
   align-items: center;
   gap: 5px;
+  font-weight: bold;
+  margin-bottom: 25px;
 `;
 const Img = styled.img`
   height: 25px;
 `;
+
+const Item = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 20px;
+  cursor: pointer;
+`;
+
 const Menu = () => {
   return (
     <Container>
@@ -27,6 +53,64 @@ const Menu = () => {
           <Img src={a} />
           AliTube
         </Logo>
+        <Item>
+          <HomeIcon />
+          Home
+        </Item>
+        <Item>
+          <ExploreOutlinedIcon />
+          Explore
+        </Item>
+        <Item>
+          <SubscriptionsOutlinedIcon />
+          Subscriptions
+        </Item>
+        <Item>
+          <VideoLibraryOutlinedIcon />
+          Library
+        </Item>
+        <Item>
+          <HistoryOutlinedIcon />
+          History
+        </Item>
+        <Item>
+          <LibraryMusicOutlinedIcon />
+          Music
+        </Item>
+        <Item>
+          <SportsEsportsOutlinedIcon />
+          Sports
+        </Item>
+        <Item>
+          <SportsBasketballOutlinedIcon />
+          Gaming
+        </Item>
+
+        <Item>
+          <MovieOutlinedIcon />
+          Movies
+        </Item>
+        <Item>
+          <ArticleOutlinedIcon />
+          News
+        </Item>
+        <Item>
+          <LiveTvOutlinedIcon />
+          Live
+        </Item>
+        <hr />
+        <Item>
+          <SettingsOutlinedIcon />
+          Settings
+        </Item>
+        <Item>
+          <FlagOutlinedIcon />
+          Report
+        </Item>
+        <Item>
+          <HelpOutlineOutlinedIcon />
+          Help
+        </Item>
       </Wrapper>
     </Container>
   );
